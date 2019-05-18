beba25eb00352261b391900025bd9a54f3d6009e
Myung Kyu Song <singsong@paran.com>
2019-05-18 17:29:19 +0900

    first commit

--- Alignment/Alignment.py
+++ Alignment/Alignment.py
@@ -1,4 +1,37 @@
+import math
+import sys
+import os
+import string
+import re
+
+class Point:
+    def __init__(self, x,y,z):
+        self.X = x
+        self.Y = y
+        self.Z = z
+
+    def CalcDist(self, pnt):
+        dis = math.sqrt(math.pow(pnt.X - self.X,2)+math.pow(pnt.Y - self.Y,2)+math.pow(pnt.Z - self.Z,2))
+        return dis
 
 
+class WorkingPoint:
+    pass
 
 
+class Tunnel:
+    pass
+
+
+def test():
+    p1 = Point(1,2,3)
+    p2 = Point(4,5,6)
+    d = Point.Dist(p1,p2)
+
+    assert d==5
+
+    print pass
+
+
+if __name__ == "__main__":
+    test()
