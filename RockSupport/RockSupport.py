cacc9c725c0738bf103feaa3b00df6f4f1e5edc9
Myung Kyu Song <singsong@paran.com>
2019-05-22 21:37:15 +0900

    first commit rock class update (5/22)

---
+++ RockSupport/RockSupport.py
@@ -0,0 +1,58 @@
+import os
+import sys
+import math
+import re
+import string
+import kivy
+import enum
+
+
+class rbType(enum.Enum):
+    rbSteelBar = 0
+    rbFRP = 1
+    rbSteelShell = 2
+
+
+class Rockbolt:
+    def __init__(self):
+        self.diameter = 0
+        self.length = 0
+        self.density = 0 # area per rockbolt
+        self.material = rbType.rbSteelBar
+
+
+class scType(enum.Enum):
+    scNormal = 0
+    scSFRC = 1
+
+
+class Shotcrete:
+    def __init__(self):
+        self.thickness = 0
+        self.type = scType.scNormal
+
+
+class RockSupport:
+    def __init__(self):
+        self.rockboltockbolt =
+        self.shotcretehotcrete =
+        pass
+
+
+class RMR:
+    def __init__(self):
+        pass
+
+
+class Qsys:
+    def __init__(self):
+        pass
+
+
+def test():
+    pass
+
+
+
+if __name__ == "__main__":
+    test()
