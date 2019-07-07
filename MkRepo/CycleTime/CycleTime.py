9b71f4182e3f56462a487bf80cf6ddc4c589f28b
mksong <singsong@paran.com>
2019-07-07 22:16:45 +0900

    commit from ubuntu 7/7

--- MkRepo/CycleTime/CycleTime.py
+++ MkRepo/CycleTime/CycleTime.py
@@ -1,56 +1,18 @@
 import sys
 sys.path.append("..")
 
-import kivy
-from Equipment.Equpiment import DummyEquipment
-
-
-class BaseCycle:
-    def __init__(self):
-        self.Name = ''
-        self.Duration = 0
-        self.Equipment = DummyEquipment()
-
-    def getName(self):
-        return self.Name
-
-    def getEquipName(self):
-        return self.Equipment.Name
-
-    def getDuration(self):
-
-
-class Mucking(BaseCycle):
-    def __init__(self):
-        super.__init__()
-        self.Name = 'Mucking'
-
-
-class Drilling(BaseCycle):
-    def __init__(self):
-        super.__init__()
-        pass
-
-
-class Blasting(BaseCycle):
-    def __init__(self):
-        super.__init__()
-        pass
-
-
-class Cycles:
-    def __init__(self):
-        self.Cycles = []
+from Cycle.Cycle import Mucking
 
 
 class CycleTime:
     def __init__(self):
-        pass
+        self.MuckingCyc = Mucking()
 
 
 
 def test():
-    pass
+    ct = CycleTime()
+    ct.MuckingCyc.set
 
 if __name__ == "__main__":
     test()
